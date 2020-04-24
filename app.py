857e4079d94ff7435671376db665d4f75ad1242e
tezign <tezign@MacBook-Pro.local>
2020-04-24 10:57:32 +0800

    add color detection

--- app.py
+++ app.py
@@ -32,14 +32,50 @@ class App(object):
           self.comment["img_size"] = Common.inspect_size(upload_img)
           st.text("意见：%s\n" % Common.parse_comment(self.comment))
 
-
-  def text_ocr(self, option):
+  '''
+  def color_detect(self, option):
       st.title(option)
+      # upload image
+      upload_img = st.file_uploader("Choose an image file(only jpg and png are supported)", type=["png", "jpg"])
+      if upload_img is not None:
+          # show original image
+          st.text("Original image:")
+          st.image(upload_img, caption=None, width=None, use_column_width=False, clamp=False, channels='RGB',
+                   format='JPEG')
+          pallette = Common.detect_main_colors(upload_img)
+          # show pallette
+          st.text("Main colors:")
+          st.image(pallette, caption=None, width=None, use_column_width=False, clamp=False, channels='RGB',
+                   format='JPEG')
+  '''
+
+  def color_detect(self, option):
+      '''
+      展示前10个色彩和对应rgb值
+      :param self:
+      :param option: 选项
+      :return:
+      '''
+      st.title(option)
+      # upload image
+      upload_img = st.file_uploader("Choose an image file(only jpg and png are supported)", type=["png", "jpg"])
+      if upload_img is not None:
+          # show original image
+          st.text("Original image:")
+          st.image(upload_img, caption=None, width=None, use_column_width=False, clamp=False, channels='RGB',
+                   format='JPEG')
+          pal_list = Common.detect_main_colors(upload_img)
+          # show pallette
+          st.text("Main colors:")
+          for i in range(len(pal_list)):
+              st.text(str(i+1) + ". 百分比：" + str(pal_list[i][0]) + " RGB色值： " + str(pal_list[i][1]))
+              st.image(pal_list[i][2], caption=None, width=None, use_column_width=False, clamp=False, channels='RGB',
+                   format='JPEG')
 
 if __name__=="__main__":
     FUNC_MAP = {
       "Logo合规性检测": 0,
-      "文字OCR": 1
+      "主色调颜色检测": 1
     }
     app = App()
     option = st.sidebar.radio('', tuple(FUNC_MAP.keys()))
@@ -47,6 +83,6 @@ if __name__=="__main__":
     if FUNC_MAP[option] == 0:
         app.logo_inspect(option)
     elif FUNC_MAP[option] == 1:
-        app.text_ocr(option)
+        app.color_detect(option)
 
 
--- models/common.py
+++ models/common.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from PIL import Image, ImageDraw
+from webcolors import rgb_to_hex
 # widgets
 class Common(object):
   def __init__(self):
@@ -19,6 +21,54 @@ class Common(object):
     else:
       return False
 
+  '''
+  合成调色板
+  @staticmethod
+  def detect_main_colors(original_image):
+      palette_height = 80
+      outline_width = 5
+      pal_unit_width = 50
+      numcolors = 10
+      original_image = Image.open(original_image)
+      colors = get_colors(original_image)  # array of colors in the image
+
+      pal = Image.new("RGB", (numcolors * (pal_unit_width), palette_height))
+      draw = ImageDraw.Draw(pal)
+      posx = 0
+
+      # making the palette
+      for col in colors:
+          draw.rectangle([posx, 0, posx + pal_unit_width, palette_height], fill=col, width=outline_width,
+                         outline="#000")
+          posx += pal_unit_width
+
+      del draw
+      return pal
+  '''
+
+  @staticmethod
+  def detect_main_colors(original_image):
+      '''
+      探测图片前10个颜色
+      :param original_image:
+      :return:
+      '''
+      pal_unit_height = 50
+      pal_unit_width = 100
+      outline_width = 3
+      original_image = Image.open(original_image)
+      colors = get_main_colors(original_image)  # array of colors in the image
+      pal_list = []
+      # draw pals
+      for per, col in colors:
+          pal = Image.new("RGB", (pal_unit_width, pal_unit_height))
+          draw = ImageDraw.Draw(pal)
+          draw.rectangle([0, 0, pal_unit_width, pal_unit_height], fill=col, width=outline_width,
+                         outline="#000")
+          pal_list.append((per, col, pal))
+      del draw
+      return pal_list
+
   @staticmethod
   def parse_comment(comment_dict):
     comment_map = {
@@ -31,3 +81,45 @@ class Common(object):
       comment += comment_map[key] + " : " + value
     return comment
 
+
+def get_main_colors(original_image):
+    '''
+    :param image:
+    :return: [(89.2%, (255,243,3)), (像素点百分比，rgb色值), ...]
+    方法一：可以先压缩（需要，不然像素点太多了），后使用getcolors（）,获取图片中的所有像素
+    '''
+    #image = original_image.resize((80, 80))
+    #colors = image.getcolors(80 * 80)  # array of colors in the image
+    width, height = original_image.size
+    colors = original_image.getcolors(width * height)
+    color_count_list = [color[0] for color in colors]
+    color_count_list.sort(reverse=True)
+    top_counts = color_count_list[:10]
+    top_colors = []
+    for color in colors:
+        if color[0] in top_counts:
+            top_colors.append(color)
+    # get top ten colors
+    top_colors = [("%.2f%%" % (float(color[0]) * 100 / (width * height)), color[1]) for color in quick_sort(top_colors)[:10]]
+    return top_colors
+
+def quick_sort(b):
+    """快速逆序排序"""
+    arr = b
+    if len(b) < 2:
+        return arr
+    # 选取基准，随便选哪个都可以，选中间的便于理解
+    mid = arr[len(b) // 2]
+    # 定义基准值左右两个数列
+    left, right = [], []
+    # 从原始数组中移除基准值
+    b.remove(mid)
+    for item in b:
+        # 大于基准值放右边
+        if item[0] >= mid[0]:
+            left.append(item)
+        else:
+            # 小于基准值放左边
+            right.append(item)
+        # 使用迭代进行比较
+    return quick_sort(left) + [mid] + quick_sort(right)
